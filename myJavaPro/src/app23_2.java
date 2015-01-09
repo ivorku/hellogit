fc8e12266c2757bf4bab1b2af8b3da6802af038b
ASAC <maria@seed.net.tw>
2015-01-09 17:52:31 +0800

    首次 commit

---
+++ myJavaPro/src/app23_2.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class app23_2  {
+
+	static JFrame frm=new JFrame("Swing: 子視窗");
+	static JButton btn=new JButton("新增視窗");
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
